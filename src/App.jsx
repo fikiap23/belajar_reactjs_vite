e5922227ee9d16c3b098f9cc375a881deb277ca3
fikiaprian23 <fikiaprian23@gmail.com>
2023-08-08 10:55:42 +0700

    buat props dengan default

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable react/prop-types */
 // function komponen
 const Button = (props) => {
+  const { children = 'ini tombol', bgcolor = 'bg-slate-700' } = props
   return (
-    <button
-      className={`${props.bgcolor} text-white py-2 px-4 rounded`}
-      type="submit"
-    >
-      {props.text}
+    <button className={`${bgcolor} text-white py-2 px-4 rounded`} type="submit">
+      {children}
     </button>
   )
 }
@@ -15,10 +13,11 @@ function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
       <div className="flex gap-x-3">
-        <Button bgcolor="bg-red-500" text="login" />
-        <Button bgcolor="bg-green-500" text="register" />
-        <Button bgcolor="bg-amber-500" text="logout" />
-        <Button bgcolor="bg-pink-500" text="profile" />
+        <Button bgcolor="bg-red-500">Logout</Button>
+        <Button bgcolor="bg-green-500">Register</Button>
+        <Button bgcolor="bg-amber-500">Login</Button>
+        <Button bgcolor="bg-pink-500">Profile</Button>
+        <Button></Button>
       </div>
     </div>
   )
