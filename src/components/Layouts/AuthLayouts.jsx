3765b03e812ea248f4297bd67c8ad60d61358525
fikiaprian23 <fikiaprian23@gmail.com>
2023-08-09 14:07:31 +0700

    ternary

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom'
+
 /* eslint-disable react/prop-types */
 const AuthLayout = (props) => {
-  const { children, title } = props
+  const { children, title, type } = props
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-xs">
@@ -9,8 +11,24 @@ const AuthLayout = (props) => {
           Welcome, Please enter your details
         </p>
         {children}
+        <p className="text-sm mt-4 text-center">
+          {type === 'login'
+            ? "Don't have an account? "
+            : 'Already have an account? '}
+          {type === 'login' && (
+            <Link to="/register" className="text-blue-600 font-bold ml-1">
+              Register
+            </Link>
+          )}
+          {type === 'register' && (
+            <Link to="/Login" className="text-blue-600 font-bold ml-1">
+              Login
+            </Link>
+          )}
+        </p>
       </div>
     </div>
   )
 }
+
 export default AuthLayout
--- src/components/Pages/register.jsx
+++ src/components/Pages/register.jsx
@@ -1,16 +1,10 @@
 import AuthLayout from '../Layouts/AuthLayouts'
 import FormRegister from '../Fragments/FormRegister'
-import { Link } from 'react-router-dom'
+
 const RegisterPage = () => {
   return (
-    <AuthLayout title="Register">
+    <AuthLayout title="Register" type="register">
       <FormRegister />
-      <p className="text-sm mt-4 text-center">
-        {`Have a account?`}
-        <Link to="/login" className="text-blue-600 font-bold ml-1">
-          Login
-        </Link>
-      </p>
     </AuthLayout>
   )
 }
