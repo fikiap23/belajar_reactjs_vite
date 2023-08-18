c1b7f9af0deed77440f52f01164dcbe98f4d5018
fikiaprian23 <fikiaprian23@gmail.com>
2023-08-18 19:11:20 +0700

    [ADD] detail product

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -10,3 +10,14 @@ export const getProducts = async (callback) => {
       console.log(err)
     })
 }
+
+export const getDetailProduct = async (id, callback) => {
+  axios
+    .get(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => {
+      callback(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
