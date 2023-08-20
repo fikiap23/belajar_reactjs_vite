40a6f5fe200a5cb53983d7c5cecf68a939eb518c
fikiaprian23 <fikiaprian23@gmail.com>
2023-08-20 13:08:18 +0700

    dark mode pake use context

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { DarkMode } from '../../context/DarkMode'
 import { useLogin } from '../../hooks/useLogin'
 import Button from '../Elements/Button'
 
@@ -7,6 +8,7 @@ const Navbar = () => {
   const username = useLogin()
   const [totalCart, setTotalCart] = useState(0)
   const cart = useSelector((state) => state.cart.data)
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode)
 
   useEffect(() => {
     if (cart.length > 0) {
@@ -26,9 +28,16 @@ const Navbar = () => {
       <Button classname="bg-red-600 ml-5" onClick={handlerLogout}>
         Logout
       </Button>
-      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5">
+
+      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5 mr-5">
         {totalCart}
       </div>
+      <Button
+        className=" bg-black px-10 mx-5 text-white rounded"
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        {isDarkMode ? 'Light' : 'Dark'}
+      </Button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import ProfilePage from './components/Pages/profile'
 import DetailProductPage from './components/Pages/detailProduct'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import Navbar from './components/Layouts/Navbar'
+import DarkModeContextProvider from './context/DarkMode'
 
 const router = createBrowserRouter([
   {
@@ -43,8 +43,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Navbar />
-      <RouterProvider router={router} />
+      <DarkModeContextProvider>
+        <RouterProvider router={router} />
+      </DarkModeContextProvider>
     </Provider>
   </React.StrictMode>
 )
